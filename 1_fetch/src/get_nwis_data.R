6e888dfdcc7f1205d72b66c859f70bf08da20564
Faunce, Kaycee Elizabeth <kfaunce@usgs.gov>
2021-10-30 13:03:29 -0400

    working towards Refactor the existing pipeline to use more effective targets #5

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,48 +1,91 @@
-
-download_nwis_data <- function(site_nums = c("01427207", "01432160", "01435000", "01436690", "01466500")){
+download_nwis_data <- function(site_num, out_dir, startDate, endDate, parameterCd){
   
-  # create the file names that are needed for download_nwis_site_data
-  # tempdir() creates a temporary directory that is wiped out when you start a new R session; 
-  # replace tempdir() with "1_fetch/out" or another desired folder if you want to retain the download
-  download_files <- file.path(tempdir(), paste0('nwis_', site_nums, '_data.csv'))
-  data_out <- data.frame()
-  # loop through files to download 
-  for (download_file in download_files){
-    download_nwis_site_data(download_file, parameterCd = '00010')
-    # read the downloaded data and append it to the existing data.frame
-    these_data <- read_csv(download_file, col_types = 'ccTdcc')
-    data_out <- bind_rows(data_out, these_data)
-  }
-  return(data_out)
-}
-
-nwis_site_info <- function(fileout, site_data){
-  site_no <- unique(site_data$site_no)
-  site_info <- dataRetrieval::readNWISsite(site_no)
-  write_csv(site_info, fileout)
-  return(fileout)
+  # function to simultaneously download site information and data for set parameter
+  
+    # create the file name needed for download_nwis_site_data
+      download_file <- file.path(out_dir, paste0("nwis_", site_num, "_nwisdata.csv"))
+        
+    # download parameter data
+      val <- retry(download_nwis_site_data(download_file, startDate, endDate, parameterCd = parameterCd), maxErrors = 10, sleep = 2)
+        
+    # download site info
+      val <- retry(download_nwis_site_info(site_num, out_dir), maxErrors = 10, sleep = 2)
+        
+      return(out_dir)
+  
 }
 
 
-download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
-  
-  # filepaths look something like directory/nwis_01432160_data.csv,
-  # remove the directory with basename() and extract the 01432160 with the regular expression match
-  site_num <- basename(filepath) %>% 
-    stringr::str_extract(pattern = "(?:[0-9]+)")
-  
-  # readNWISdata is from the dataRetrieval package
-  data_out <- readNWISdata(sites=site_num, service="iv", 
-                           parameterCd = parameterCd, startDate = startDate, endDate = endDate)
 
-  # -- simulating a failure-prone web-sevice here, do not edit --
-  set.seed(Sys.time())
-  if (sample(c(T,F,F,F), 1)){
-    stop(site_num, ' has failed due to connection timeout. Try tar_make() again')
-  }
-  # -- end of do-not-edit block
+download_nwis_site_info <- function(site_num, out_dir){ 
   
-  write_csv(data_out, file = filepath)
-  return(filepath)
+  # function to access and download site information for set site number
+      
+      site_info <- dataRetrieval::readNWISsite(site_num)
+      readr::write_csv(site_info, paste0(out_dir, "nwis_", site_num, "_siteinfo.csv"))
+      return(paste0(out_dir, "nwis_", site_num, "_siteinfo.csv"))
+  
 }
 
+
+
+download_nwis_site_data <- function(filepath, parameterCd, startDate, endDate){ 
+  
+  # function to access and download data for set parameter
+  
+      site_num <- basename(filepath) %>% 
+        stringr::str_extract(pattern = "(?:[0-9]+)")
+      
+      data_out <- dataRetrieval::readNWISdata(
+        sites=site_num, 
+        service="iv", 
+        parameterCd = parameterCd, 
+        startDate = startDate, 
+        endDate = endDate
+        )
+  
+    # -- simulating a failure-prone web-service here, do not edit --
+      set.seed(Sys.time())
+      if (sample(c(T,F,F,F), 1)){
+        stop(site_num, ' has failed due to connection timeout. Try tar_make() again')
+      }
+    # -- end of do-not-edit block
+    
+      readr::write_csv(data_out, filepath)
+      return(filepath)
+    
+}
+
+
+
+retry <- function(expr, isError=function(x) "try-error" %in% class(x), maxErrors, sleep) { 
+  
+  # function to retry downloads within set number of attempts and wait period
+  
+      attempts = 0
+      retval = try(eval(expr))
+      
+      while (isError(retval)) {
+        
+        attempts = attempts + 1
+        if (attempts >= maxErrors) {
+          msg = sprintf("retry: too many retries [[%s]]", capture.output(str(retval)))
+          flog.fatal(msg)
+          stop(msg)
+          
+        } else {
+          
+          msg = sprintf("retry: error in attempt %i/%i [[%s]]", attempts, maxErrors, capture.output(str(retval)))
+          futile.logger::flog.error(msg)
+          warning(msg)
+          
+        }
+        
+        if (sleep > 0) Sys.sleep(sleep)
+        retval = try(eval(expr))
+        
+      }
+      
+      return(retval)
+  
+}
