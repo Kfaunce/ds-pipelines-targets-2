a3e66579fd316aecc3806a17515c86f4b3d64d03
Faunce, Kaycee Elizabeth <kfaunce@usgs.gov>
2022-01-06 12:50:16 -0500

    Fix get_site_info_data function - pass site_list into function and add to target

--- 1_fetch/src/get_site_info_data.R
+++ 1_fetch/src/get_site_info_data.R
@@ -1,11 +1,9 @@
-get_site_info_data <- function(eval_data){ 
+get_site_info_data <- function(eval_data, site_list){ 
   
   # function to access and download site information based on unique site numbers
-  
-      sites <- unique(eval_data$site_no)
-  
+
     # download site info - loop through sites and append to a single dataframe
-      data_out <- retry(download_nwis_site_info(sites), maxErrors = 10, sleep = 2)
+      data_out <- retry(download_nwis_site_info(site_list), maxErrors = 10, sleep = 2)
       
       return(data_out)
   
